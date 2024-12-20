9ebcadac6c5f0f45b42460b1e6e04988d245506e
Jit <jit.oung254343@gmail.com>
2024-12-20 23:28:02 +0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        <div>Movie List Section</div>
+        <div>
+        {movies.map((item, index) => (
+            <div  className="movieBox" key={index}>
+              <div>Title : {item.title}</div>
+              <div>Year : {item.year}</div>
+              <div>Runtime : {item.runtime}</div>
+              <div>Director : {item.director}</div>
+              <div>Genres : {item.genres}</div>
+              <div>imdbRating : {item.imdbRating}</div>
+              <div>imdbVotes : {item.imdbVotes}</div>
+              <div  >
+                <img  className="photo" src={item.image}></img>
+               
+              </div>            
+            </div>
+          ))}
+         </div>
       </section>
     </div>
   );
